85b2bd048818c9153d82749a66b9c76ca755e53e
Sujaltalreja29 <sujaltlrj@gmail.com>
2025-01-10 22:58:00 +0530

    solved auth error

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -30,9 +30,10 @@ export default function SignInForm() {
 
   const { toast } = useToast();
   const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    console.log(data)
     const result = await signIn('credentials', {
       redirect: false,
-      identifier: data.identifier,
+      email: data.identifier,
       password: data.password,
     });
     console.log(result);
